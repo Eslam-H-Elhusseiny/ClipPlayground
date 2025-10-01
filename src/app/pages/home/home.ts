a4739470f1fd490a8bf14a00c076000119753910
Eslam Elhusseiny <eslam.hamed.elhusseiny@gmail.com>
2025-10-01 03:28:13 +0300

    Fix home page upload button to conditionally route based on authentication state

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgxLavaLampComponent } from '@omnedia/ngx-lava-lamp';
 import { RouterLink } from '@angular/router';
 import { ClipList } from '../../shared/layout/clip-list/clip-list';
+import { AuthStore } from '../../shared/services/auth-store';
+import { AsyncPipe } from '@angular/common';
+import { ModalManager } from '../../shared/services/modal-manager';
 
 @Component({
   selector: 'app-home',
-  imports: [NgxLavaLampComponent, RouterLink, ClipList],
+  imports: [NgxLavaLampComponent, RouterLink, ClipList, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  auth = inject(AuthStore);
+  modalManager = inject(ModalManager);
+
+  openModal($event: Event) {
+    $event.preventDefault();
+
+    this.modalManager.toggleModal('auth');
+  }
+}
