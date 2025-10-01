32609cd892bad84f8083548ea4aa7571f2107f97
Eslam Elhusseiny <eslam.hamed.elhusseiny@gmail.com>
2025-10-01 03:12:59 +0300

    Add Firebase configuration to environment files for deployment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD7_pO2tAunFqZmQ372yqn5_soY5lnIv18',
+    authDomain: 'clipplayground.firebaseapp.com',
+    projectId: 'clipplayground',
+    storageBucket: 'clipplayground.firebasestorage.app',
+    messagingSenderId: '540632334894',
+    appId: '1:540632334894:web:61f15b65eb387b1231d5bf',
+  },
+};
